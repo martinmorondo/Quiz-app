4122dc708910e8f46ab656b46047a853392cfc7f
Martín Morondo <martinmorondo@gmail.com>
2023-05-29 17:06:01 -0300

    add comments for code understanding and functionality random questions

--- JS/main.js
+++ JS/main.js
@@ -56,29 +56,31 @@ const aText = document.getElementById('a-text');
 const bText = document.getElementById('b-text');
 const cText = document.getElementById('c-text');
 const dText = document.getElementById('d-text');
+const errorMessage = document.getElementById('error-message');
 const submitBtn = document.getElementById('submit');
 
 let currentQuestion = 0;
 let score = 0;
+shuffle(quizData); // Randomizar el orden de las preguntas
 
 loadQuiz();
 
 function loadQuiz() {
     deselectAnswers();
 
-
+// Obtener los datos de la pregunta actual
 const currentQuizData = quizData[currentQuestion];
 
+// Mostrar la pregunta y las opciones en el HTML
 questionEl.innerText = currentQuizData.question;
-
 aText.innerText = currentQuizData.a;
 bText.innerText = currentQuizData.b;
 cText.innerText = currentQuizData.c;
 dText.innerText = currentQuizData.d;
-
 }
 
 function deselectAnswers() {
+    // Deseleccionar todas las respuestas
     answerEls.forEach(answerEl => 
         answerEl.classList.checked = false);
 }
@@ -86,6 +88,7 @@ function deselectAnswers() {
 function getSelected() {
     let answer;
 
+// Obtener la respuesta seleccionada por el usuario
     answerEls.forEach(answerEl => {
         if (answerEl.checked) {
             answer = answerEl.id;
@@ -97,23 +100,41 @@ function getSelected() {
 submitBtn.addEventListener('click', () => {
     const answer = getSelected();
 
-    if (answer === quizData[currentQuestion].correct) {
-        score++;
+    if (answer) { // Verifica si se ha seleccionado una respuesta
+        if (answer === quizData[currentQuestion].correct) {
+            score++;
+        }
+        currentQuestion++;
+
+        if (currentQuestion < quizData.length) {
+            // Cargar la siguiente pregunta
+            loadQuiz();
+        } else {
+            // Mostrar resultados y botón de reinicio
+            quizContainer.innerHTML = `
+            <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+            <button class='btn-restart' onclick="location.reload()">Restart</button>
+            `;
+        }
+    } else {
+        // Mensaje de error para el usuario
+        errorMessage.textContent = 'Please select an answer before submitting.';
     }
-    currentQuestion++;
-
-    if (currentQuestion < quizData.length) {
-        loadQuiz();
-} else {
-    quiz.innerHTML = `
-    <h2>Yoy answeredd ${score}/${quizData.length} questions correctyl</h2>
-
-    <button onclick = "location.reload()">Reload</button>
-    `
-}
 });
 
+function shuffle(array) {
+    let currentIndex = array.length;
 
+    while (currentIndex !== 0) {
+        const randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // Intercambiar elementos
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
 
 
 
